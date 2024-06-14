56308c7017404d3cf1f893d3716539840015fff7
Clement-Codes <clementcodes@gmail.com>
2024-06-15 00:17:17 +0100

    update

--- Linked List/SingleLinkedList.cpp
+++ Linked List/SingleLinkedList.cpp
@@ -7,13 +7,21 @@ class SingleLinkedList{
     SingleLinkedList* next;
 
     public:
-    SingleLinkedList(int data1){
+    SingleLinkedList(string data1){
         data = data1;
         next = nullptr;
     }
 };
 
-bool search(SingleLinkedList)
+bool search(SingleLinkedList* node, string target){
+    while(node->next != nullptr){
+        if(node->data == target){
+            return true;
+        }
+        node = node->next;
+    }
+    return false;
+}
 
 void traverse(SingleLinkedList* node){
     while(node!=nullptr){
@@ -22,12 +30,47 @@ void traverse(SingleLinkedList* node){
     }
 }
 
+int length (SingleLinkedList* node){
+    int temp = 0;
+    while (node->next != nullptr)
+    {
+        temp+=1;
+        node=node->next;
+    }
+    return temp;
+}
+
+void insertionAtBeginning(SingleLinkedList** head, string data){
+    SingleLinkedList* temp = new SingleLinkedList(data);
+    temp->next = *head;
+    *head = temp;
+}
+
+void insertionAtEnd(SingleLinkedList** Node, string data){
+    SingleLinkedList* temp = new SingleLinkedList(data);
+    SingleLinkedList* head_temp = *Node;
+    if ((*Node) == nullptr){
+        (*Node) = temp;
+    }
+    else{
+        while(head_temp->next != nullptr){
+            head_temp = head_temp->next;
+        }
+        head_temp->next = temp;
+    }
+}
+
 int main(){
-    SingleLinkedList* a =  new SingleLinkedList('A');
-    SingleLinkedList* b =  new SingleLinkedList('B');
-    SingleLinkedList* c =  new SingleLinkedList('C');
+    SingleLinkedList* a =  new SingleLinkedList("A");
+    SingleLinkedList* b =  new SingleLinkedList("B");
+    SingleLinkedList* c =  new SingleLinkedList("C");
     a->next = b;
     b->next = c;
+    traverse(a);
+    cout<<search(a,"D")<<endl;
+    cout<<"Length: "<<length(a)<<endl;
+    insertionAtBeginning(&a,"D");
+    insertionAtEnd(&a,"D");
     traverse(a);
     return 0;
 }
